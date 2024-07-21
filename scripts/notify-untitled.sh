324ce63d633861427bde4872878dc4a19c5efd0c
Quantiux <roy@quantiux.com>
2024-07-20 20:47:19 -0400

    Minor edit

--- scripts/notify-untitled.sh
+++ scripts/notify-untitled.sh
@@ -4,11 +4,7 @@
 # https://www.linuxquestions.org/questions/blog/the-dsc-472367/for-those-who-dont-like-notify-send-exhibiting-its-title-notify-send-on-lxqt-38940/
 
 if [[ ! "${@}" =~ "-a " ]] ; then
-
-/usr/bin/notify-send -a "" "${@}"
-
+    /usr/bin/notify-send -a "" "${@}"
 else
-
-/usr/bin/notify-send "${@}"
-
+    /usr/bin/notify-send "${@}"
 fi
